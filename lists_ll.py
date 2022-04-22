b15ff52ced5ef1c3db647e869eb91ce6be2672b6
Alaina Rosalez <alainarosalez@gmail.com>
2022-04-22 13:00:42 -0400

    Lists Lesson 7

--- lists_ll.py
+++ lists_ll.py
@@ -166,3 +166,39 @@ range_five_three = range(5, 15, 3)
 #~ 2
 range_diff_five = range(0, 40, 5)
 
+#todo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#! Length
+
+#& Often, we’ll need to find the number of items in a list, usually called its length.
+#& We can do this using a built-in function called len().
+#& When we apply len() to a list, we get the number of elements in that list:
+#* my_list = [1, 2, 3, 4, 5]
+#* print(len(my_list))
+#& Would output:
+#* 5
+
+#~ 1
+long_list = [1, 5, 6, 7, -23, 69.5, True, "very", "long", "list", "that", "keeps", "going.", "Let's", "practice", "getting", "the", "length"]
+big_range = range(2, 3000, 10)
+# Your code below: 
+long_list_len = len(long_list)
+
+#~ 2
+print(long_list_len)
+
+#~ 3
+big_range_length = len(big_range)
+
+#~ 4
+print(big_range_length)
+
+#~ 5
+long_list = [1, 5, 6, 7, -23, 69.5, True, "very", "long", "list", "that", "keeps", "going.", "Let's", "practice", "getting", "the", "length"]
+big_range = range(2, 3000, 100)
+# Your code below: 
+long_list_len = len(long_list)
+print(long_list_len)
+big_range_length = len(big_range)
+print(big_range_length)
+
